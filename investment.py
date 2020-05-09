6ff7092d49e570516edd98d5faf03c3b93435515
Ian <ianyang.sg@gmail.com>
2020-05-09 21:12:03 +0800

    calculate the daily compounded return for HK and US portfolio

--- investment.py
+++ investment.py
@@ -4,17 +4,37 @@ from yahoofinancials import YahooFinancials
 
 
 # %%
-ticker = 'AAPL'
-yahoo_financials = YahooFinancials(ticker)
+def get_price_table(ticker, start_date, end_date, frequency):
+    yahoo_financials = YahooFinancials(ticker)
+    df = pd.DataFrame(yahoo_financials.get_historical_price_data(
+        start_date, end_date, frequency)[ticker]['prices'])
+    df['formatted_date'] = pd.to_datetime(df['formatted_date'])
+    df.set_index('formatted_date', inplace=True)
+    df.index.name = 'date'
+    return df
 
 
 # %%
-df = pd.DataFrame(yahoo_financials.get_historical_price_data(
-    '2018-01-01', '2019-03-19', 'daily')[ticker]['prices'])
-
+ticker_US = 'SPY'
+ticker_HK = '2800.HK'
+start_date = '2015-01-01'
+end_date = '2019-12-31'
+frequency = 'daily'
+weight_US = 0.8
+weight_HK = 0.2
 
 # %%
-df.set_index('formatted_date', inplace=True)
+df_US = get_price_table(ticker_US, start_date, end_date, frequency)
+df_HK = get_price_table(ticker_HK, start_date, end_date, frequency)
 
 # %%
-df.head()
+df_portfolio = (pd.merge(df_US, df_HK, how='outer', left_index=True,
+                         right_index=True, suffixes=('_US', '_HK'))
+                [['adjclose_US', 'adjclose_HK']].fillna(method='ffill'))
+
+# %%
+df_portfolio[['US_return', 'HK_return']] = df_portfolio.pct_change()
+
+# %%
+df_portfolio['total_return'] = weight_US * \
+    df_portfolio['US_return']+weight_HK*df_portfolio['HK_return']
